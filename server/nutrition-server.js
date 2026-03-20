e3fd9bc3f233912abba4bcc6674c5807f2df82aa
nadfk <naaddiiaakusuma@gmail.com>
2026-03-21 02:54:57 +0700

    draft server nutrition

--- server/nutrition-server.js
+++ server/nutrition-server.js
@@ -1,6 +1,5 @@
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
-const { MAX } = require('uuid');
 
 const packageDef = protoLoader.loadSync('proto/nutrition.proto');
 const proto = grpc.loadPackageDefinition(packageDef).nutrition;
